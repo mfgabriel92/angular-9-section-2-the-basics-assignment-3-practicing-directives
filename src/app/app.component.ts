f59166563af22672e8a6d4fe6e5d72352eabccbf
Gabriel M. Fernandes <mfgabriel92@gmail.com>
2020-02-16 08:22:46 -0300

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = 'section-ii-assignment-iii';
+  showDetails: boolean = false;
+  dates: Array<String> = [];
+
+  onClick(): void {
+    this.showDetails = !this.showDetails;
+
+    let d = new Date();
+    this.dates.push(
+      d.getDay() +
+        "/" +
+        d.getMonth() +
+        "/" +
+        d.getFullYear() +
+        " - " +
+        d.getHours() +
+        ":" +
+        d.getMinutes() +
+        ":" +
+        d.getSeconds()
+    );
+  }
 }
